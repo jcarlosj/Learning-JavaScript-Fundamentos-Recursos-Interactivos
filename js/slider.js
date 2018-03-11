c4d9cba3c9d924658635b8f14e9104868c48d573
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-03-11 10:55:31 -0500

    Cambia estado del paginador del Slider a su item activo

--- js/slider.js
+++ js/slider.js
@@ -15,15 +15,25 @@ var slider = {
     // Recorre cada uno de los elementos que representan cada una de las diapositivas del Slider y asigna dinámicamente el evento click a cada uno de ellos
     slider .paginador .forEach( ( slide ) => {
       slide .addEventListener( 'click', slider .accionPaginador );      // Agrega el evento 'click' a cada uno de los elementos
+
     });
   },
   accionPaginador : function ( event ) {
     slider .pagina = event .target. parentNode .getAttribute( 'data-page' );    // Captura el atributo otorgado a la propiedad 'data-page' del elemento padre que posee el manejador (span)
+    slider .cambiaEstadoPaginador( event );
     slider .mover( slider .pagina );                                            // Cambia el Slide
   },
   mover : function( pagina ) {
     console .log( ( 'left: ', pagina * 100 * -1 ) + '%' );
     slider .contenedor .style .left = ( pagina * 100 * -1 ) + '%';
+  },
+  cambiaEstadoPaginador : function ( event ) {
+    // Recorre cada uno de los elementos que representan las páginas del Slider y cambia el atributo CSS de opacidad
+    slider .paginador .forEach( ( item_pagina ) => {
+      item_pagina .style .opacity = .5;
+    });
+    // Cambia el atributo de opacidad al elemento actual o activo
+    event .target .parentNode .style .opacity = 1;
   }
 }
 
