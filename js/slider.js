6464d24f4d0ff2d7f3af32eedd5cee8a9808c0ec
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-03-11 10:23:35 -0500

    Despliega un 'Slide' por cada evento del paginador
    Aclaración
     - El movimiento inicial de cada uno de los 'slides' es negativo
       por eso se hace negativo el valor multiplicando por -1

--- js/slider.js
+++ js/slider.js
@@ -3,19 +3,27 @@
 // Objecto Implicito
 var slider = {
   paginador: null,
+  pagina: null,
+  contenedor: null,
   inicio : function() {
     slider .paginacion();
   },
   paginacion : function() {
     slider .paginador = document .querySelectorAll( '#pager li' );      // Obtiene todos los elementos que componen el paginador del Slider
+    slider .contenedor = document .querySelector( '#slider ul' );       // Obtiene el contenedor donde se despliegan cada uno de los Slides
 
     // Recorre cada uno de los elementos que representan cada una de las diapositivas del Slider y asigna dinámicamente el evento click a cada uno de ellos
     slider .paginador .forEach( ( slide ) => {
-      slide .addEventListener( 'click', slider .accionPaginador );
+      slide .addEventListener( 'click', slider .accionPaginador );      // Agrega el evento 'click' a cada uno de los elementos
     });
   },
   accionPaginador : function ( event ) {
-    console .log( event .target. parentNode .getAttribute( 'data-page' ) );
+    slider .pagina = event .target. parentNode .getAttribute( 'data-page' );    // Captura el atributo otorgado a la propiedad 'data-page' del elemento padre que posee el manejador (span)
+    slider .mover( slider .pagina );                                            // Cambia el Slide
+  },
+  mover : function( pagina ) {
+    console .log( ( 'left: ', pagina * 100 * -1 ) + '%' );
+    slider .contenedor .style .left = ( pagina * 100 * -1 ) + '%';
   }
 }
 
