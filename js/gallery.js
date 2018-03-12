306012aea7042a93d43c77a6fc439ab7ab973849
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-03-12 14:59:19 -0500

    Lanza Lightbox al hacer 'click' sobre cualquier elemento 'img' de la Galería

--- js/gallery.js
+++ js/gallery.js
@@ -4,6 +4,8 @@
 var gallery = {
   imagenes: null,
   pathImage: null,
+  elementBody: null,
+  lightbox: null,
   /* Inicializa las funcionalidades de la Galeria */
   inicio: function() {
     gallery .imagenes = document .querySelectorAll( '#gallery ul li img' );        // Obtiene todas las imágenes de la galería
@@ -16,7 +18,21 @@ var gallery = {
   /* Captura el elemento que contiene la imagen */
   captura : function ( event ) {
     gallery .pathImage = event .target;
-    console .log( gallery .pathImage );
+    gallery .efecto_lightbox( gallery .pathImage );
+  },
+  /* Lanza el Lightbox */
+  efecto_lightbox : function( elementImg ) {
+    gallery .elementBody = document .querySelector( 'body' );                                                     // Obtiene el elemento 'body' del DOM
+    gallery .elementBody .appendChild( document .createElement( 'div' ) ) .setAttribute( 'id', 'lightbox' );      // Crea y Agrega un elemento 'div' dentro del elemento 'body del DOM y Agrega un atributo 'id' al elemento creado
+    gallery .lightbox = document .querySelector( '#lightbox' );                                                   // Obtiene el elemento 'div' creado con el 'id': 'lightbox'
+
+    gallery .lightbox .style .width = '100%';
+    gallery .lightbox .style .height = '100%';
+    gallery .lightbox .style .position = 'fixed';
+    gallery .lightbox .style .zIndex = '10';
+    gallery .lightbox .style .background = 'rgba( 0, 0, 0, .8 )';
+    gallery .lightbox .style .top = 0;
+    gallery .lightbox .style .left = 0;
   }
 }
 
