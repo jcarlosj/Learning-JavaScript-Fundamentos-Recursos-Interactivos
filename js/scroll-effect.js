a92f345c768d344fdd9d4be1de5f1adb42deaf72
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-03-19 18:17:07 -0500

    Agrega 'Efecto Parallax' a los artículos al hacer 'scroll' con el 'mouse'

--- js/scroll-effect.js
+++ js/scroll-effect.js
@@ -4,15 +4,33 @@
 var scrollEffect = {
   /* Atributos */
   desplazamientoEnY: 0,
+  articulos: null,
+  elementScrollEffect: null,
   /* Inicializa las funcionalidades del 'Efecto Scroll' */
   inicio : function() {
+    scrollEffect .elementScrollEffect = document .querySelector( '#scroll-effect' );    // Obtiene el elemento con 'id':'scroll-effect'
+    scrollEffect .articulos = document .querySelectorAll( '#scroll-effect article' );   // Obtiene todos los elementos 'article' contendos en el elemento con 'id':'scroll-effect'
+
     // 'scroll' aplica este evento para detectar el Scroll del Mouse sobre el DOM
     document .addEventListener( 'scroll', scrollEffect .parallaxEffect );
   },
+  /* Implementa el Efecto Parallax a los artículos contenidos dentro del elemento 'scroll-effect' */
   parallaxEffect : function() {
-    // 'window.pageYOffset' es un alias de 'scrollY' y devuelve al número de pixeles desplazados verticamente en el espacio del documento
-    scrollEffect .desplazamientoEnY = window .pageYOffset;
+    scrollEffect .desplazamientoEnY = window .pageYOffset;                              // 'window.pageYOffset' es un alias de 'scrollY' y devuelve al número de pixeles desplazados verticamente en el espacio del documento
     console .log( scrollEffect .desplazamientoEnY );
+
+    // Valida si el desplazamiento actual del 'scroll' es mayor a la distancia superior con su elemento padre
+    if( scrollEffect .desplazamientoEnY > scrollEffect .elementScrollEffect .offsetTop - 200  ) {       // Elimina 200 pixeles de la altura superior con el elemento padre para queel desplazamiento ocurra más rápido
+      scrollEffect .articulos .forEach( ( articulo ) => {
+        articulo .style .marginLeft = '0';
+      });
+    }
+    else {
+      scrollEffect .articulos .forEach( ( articulo ) => {
+        articulo .style .marginLeft = scrollEffect .desplazamientoEnY / 25 - 100 + '%';
+      });
+    }
+    /* NOTA: 'offsetTop' Retorna la distancia del elemento actual respecto al borde superior del nodo padre ('offsetParent') */
   }
 }
 
