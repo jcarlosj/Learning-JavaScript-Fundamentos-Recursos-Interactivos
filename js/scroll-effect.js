a9e5dbc83d53a774bee31fa14eca1441e0f1c0a4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-03-19 18:35:34 -0500

    Fija encabezado (logo, menú) en la parte superior cuando se hace 'scroll' con el 'mouse'

--- js/scroll-effect.js
+++ js/scroll-effect.js
@@ -6,8 +6,10 @@ var scrollEffect = {
   desplazamientoEnY: 0,
   articulos: null,
   elementScrollEffect: null,
+  elementHeader: null,
   /* Inicializa las funcionalidades del 'Efecto Scroll' */
   inicio : function() {
+    scrollEffect .elementHeader = document .querySelector( 'header' );                  // Obtiene el elemento 'header' del DOM
     scrollEffect .elementScrollEffect = document .querySelector( '#scroll-effect' );    // Obtiene el elemento con 'id':'scroll-effect'
     scrollEffect .articulos = document .querySelectorAll( '#scroll-effect article' );   // Obtiene todos los elementos 'article' contendos en el elemento con 'id':'scroll-effect'
 
@@ -18,6 +20,16 @@ var scrollEffect = {
   parallaxEffect : function() {
     scrollEffect .desplazamientoEnY = window .pageYOffset;                              // 'window.pageYOffset' es un alias de 'scrollY' y devuelve al número de pixeles desplazados verticamente en el espacio del documento
     console .log( scrollEffect .desplazamientoEnY );
+
+    // Valida si el deplazamiento es mayor al alto del elemento 'header' (Contenedor del: Logo y el Menú)
+    if( scrollEffect .desplazamientoEnY > scrollEffect .elementHeader .clientHeight ) {
+      scrollEffect .elementHeader .style .position = 'fixed';
+      scrollEffect .elementHeader .style .zIndex = '10';
+    }
+    else {
+      scrollEffect .elementHeader .style .position = 'relative';
+      scrollEffect .elementHeader .style .zIndex = '0';
+    }
 
     // Valida si el desplazamiento actual del 'scroll' es mayor a la distancia superior con su elemento padre
     if( scrollEffect .desplazamientoEnY > scrollEffect .elementScrollEffect .offsetTop - 200  ) {       // Elimina 200 pixeles de la altura superior con el elemento padre para queel desplazamiento ocurra más rápido
