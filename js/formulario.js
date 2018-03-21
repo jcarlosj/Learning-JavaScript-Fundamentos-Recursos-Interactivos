58a00455b560c087f8dd1ceec5fe3b8b6246839e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-03-21 13:24:35 -0500

    Implementa evento 'blur' a los campos: nombre, contraseña y correo (elimina mensaje de error ante el campo vacío)

--- js/formulario.js
+++ js/formulario.js
@@ -10,7 +10,8 @@ var formulario = {
 
     // Recorre y aplica el evento 'focus' a cada uno de los campos obtenidos
     formulario .campos_texto .forEach( ( entrada ) => {
-      entrada .addEventListener( 'focus', formulario .campoEnFoco );
+      entrada .addEventListener( 'focus', formulario .campoEnFoco );            // 'blur' evento que se dispara cuando un campo ha recibido su foco
+      entrada .addEventListener( 'blur', formulario .campoFueraFoco );          // 'blur' evento que se dispara cuando un campo ha perdido su foco
     });
   },
   /* Función que realiza una acción cuando el campo esta en foco */
@@ -28,6 +29,21 @@ var formulario = {
       document .querySelector( `[for=${ event .target .id }] .obligatorio` ) .style .opacity = 1;
       document .querySelector( `#${ event .target .id }` ) .style .background = 'rgba( 255, 255, 0, .5 )';
     }
+  },
+  /* Función que realiza una acción cuando el campo esta en foco */
+  campoFueraFoco : function( event ) {
+    console .group( 'Pierde Foco en el campo con' );
+      console .log( 'id: ', event .target .id );
+      console .log( 'value: ', event .target .value );
+    console .groupEnd();
+
+    // Asigna el valor que tiene el campo
+    valor_campo_texto = event .target .value;
+
+    // Selecciona el elemento con el 'class': 'obligatorio' para que lo muestre si el campo esta vacío
+    document .querySelector( `[for=${ event .target .id }] .obligatorio` ) .style .opacity = 0;
+    document .querySelector( `#${ event .target .id }` ) .style .background = 'white';
+
   }
 }
 
