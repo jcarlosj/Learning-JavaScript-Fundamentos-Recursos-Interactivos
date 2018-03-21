6766877c1ef028a8951a415aee8d142de804d35f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-03-21 18:57:33 -0500

    Implementa evento 'change' a los campos: nombre, contraseña y correo (lanza mensaje de error)

--- js/formulario.js
+++ js/formulario.js
@@ -12,6 +12,7 @@ var formulario = {
     formulario .campos_texto .forEach( ( entrada ) => {
       entrada .addEventListener( 'focus', formulario .campoEnFoco );            // 'blur' evento que se dispara cuando un campo ha recibido su foco
       entrada .addEventListener( 'blur', formulario .campoFueraFoco );          // 'blur' evento que se dispara cuando un campo ha perdido su foco
+      entrada .addEventListener( 'change', formulario .cambiaEntradaCampo );    // 'change' evento que se dispara cuando un campo de tipo input, select o textarea han cambiado su valor
     });
   },
   /* Función que realiza una acción cuando el campo esta en foco */
@@ -29,6 +30,9 @@ var formulario = {
       document .querySelector( `[for=${ event .target .id }] .obligatorio` ) .style .opacity = 1;
       document .querySelector( `#${ event .target .id }` ) .style .background = 'rgba( 255, 255, 0, .5 )';
     }
+
+    // Crea elemento personalizado para los mensajes de error
+    document .querySelector( `[for=${ event .target .id }]` ) .appendChild( document .createElement( 'div' ) ) .setAttribute( 'class', 'error' );
   },
   /* Función que realiza una acción cuando el campo esta en foco */
   campoFueraFoco : function( event ) {
@@ -44,6 +48,51 @@ var formulario = {
     document .querySelector( `[for=${ event .target .id }] .obligatorio` ) .style .opacity = 0;
     document .querySelector( `#${ event .target .id }` ) .style .background = 'white';
 
+  },
+  cambiaEntradaCampo : function( event ) {
+    console .group( 'Pierde Foco en el campo con' );
+      console .log( 'id: ', event .target .id );
+      console .log( 'value: ', event .target .value );
+    console .groupEnd();
+
+    // Asigna el valor que tiene el campo
+    valor_campo_texto = event .target .value;
+
+    // Valida que el campo no esté vacío
+    if( valor_campo_texto != '' ) {
+      console .log( 'No esta vacio' );
+      // Valida si la entrada es válida
+      if( event .target .id == 'nombre' ) {
+        // Valida que la longitud del valor sea inferior a 2 ni mayor a 6 caracteres
+        if( valor_campo_texto .length < 2 || valor_campo_texto .length > 6 ) {
+          // Agrega elemento que lanza mensaje de ERROR
+          document .querySelector( `[for=${ event .target .id }] .error` ) .innerHTML = '<span style="color: red;"> * Error: El nombre de usuario debe tener entre 2 y 6 caracteres</span>';
+        }
+        else {
+          // Elimina elemento que contiene el mensaje de ERROR
+          document .querySelector( `[for=${ event .target .id }] .error` ) .parentNode .removeChild( document .querySelector( `[for=${ event .target .id }] .error` ) );
+        }
+      }
+
+      // Valida si la entrada es válida
+      if( event .target .id == 'clave' ) {
+        // Valida que la longitud del valor sea inferior a 8 ni mayor a 16 caracteres
+        if( valor_campo_texto .length < 8 || valor_campo_texto .length > 16 ) {
+          // Agrega elemento que lanza mensaje de ERROR
+          document .querySelector( `[for=${ event .target .id }] .error` ) .innerHTML = '<span style="color: red;"> * Error: El nombre de usuario debe tener entre 8 y 16 caracteres</span>';
+        }
+        else {
+          // Elimina elemento que contiene el mensaje de ERROR
+          document .querySelector( `[for=${ event .target .id }] .error` ) .parentNode .removeChild( document .querySelector( `[for=${ event .target .id }] .error` ) );
+        }
+      }
+
+      // Valida si la entrada es válida
+      if( event .target .id == 'email' ) {
+
+      }
+
+    }
   }
 }
 
