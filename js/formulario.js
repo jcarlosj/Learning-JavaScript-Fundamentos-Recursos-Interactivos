c7fb70d6391ef53cbc245450673714cdc1d341b4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-03-29 16:59:12 -0500

    Valida campos obligatorios y aceptación de 'Términos y Condiciones' para envio de datos

--- js/formulario.js
+++ js/formulario.js
@@ -6,8 +6,23 @@ var formulario = {
   campos_texto: null,
   valor_campo_texto: null,
   regExp: null,
+  validaUsuario: false,
+  validaClave: false,
+  validaEmail: false,
+  validaTerminosYCondiciones: false,
+  id: null,
+  btnEnviar: null,
+  /* Función que inicializa la funcionalidad del Formulario */
   inicio : function() {
     formulario .campos_texto = document .querySelectorAll( '#formulario-home input.campos_texto' );     // Obtiene todos los campos input donde se ingresa texto
+    formulario .id = document .querySelector( '#formulario-registro' );                                 // Obtiene el elemento 'form' del formulario a través de su ID.
+    formulario .btnEnviar = document .querySelector( '#btn-enviar' );                                   // Obtiene el elemento 'button' del formulario a través de su ID.
+
+    // Evento al elemento 'button' del formulario
+    formulario .btnEnviar .addEventListener( 'click', formulario .validaFormulario );
+
+    // Limpia los campos
+    formulario .id .reset();
 
     // Recorre y aplica el evento 'focus' a cada uno de los campos obtenidos
     formulario .campos_texto .forEach( ( entrada ) => {
@@ -50,6 +65,7 @@ var formulario = {
     document .querySelector( `#${ event .target .id }` ) .style .background = 'white';
 
   },
+  /* Función que realiza validacion de los campos cuando se producen cambios en los valores del formulario */
   cambiaEntradaCampo : function( event ) {
     console .group( 'Pierde Foco en el campo con' );
       console .log( 'id: ', event .target .id );
@@ -68,10 +84,12 @@ var formulario = {
         if( valor_campo_texto .length < 2 || valor_campo_texto .length > 6 ) {
           // Agrega elemento que lanza mensaje de ERROR
           document .querySelector( `[for=${ event .target .id }] .error` ) .innerHTML = '<span style="color: red;"> * Error: El nombre de usuario debe tener entre 2 y 6 caracteres</span>';
+          formulario .validaUsuario = false;        // Cambia el estado de validación del campo
         }
         else {
           // Elimina elemento que contiene el mensaje de ERROR
           document .querySelector( `[for=${ event .target .id }] .error` ) .parentNode .removeChild( document .querySelector( `[for=${ event .target .id }] .error` ) );
+          formulario .validaUsuario = true;         // Cambia el estado de validación del campo
         }
       }
 
@@ -84,10 +102,12 @@ var formulario = {
         if( !formulario .regExp .test( valor_campo_texto ) ) {
           // Hay un error en el formato de la contraseña
           document .querySelector( `[for=${ event .target .id }] .error` ) .innerHTML = '<span style="color: red;"> * Error: La contraseña debe tene mínimo 8 caracteres e incluir: letras mayúsculas, minúsculas y números</span>';
+          formulario .validaClave = false;          // Cambia el estado de validación del campo
         }
         else {
           // Elimina elemento que contiene el mensaje de ERROR
           document .querySelector( `[for=${ event .target .id }] .error` ) .parentNode .removeChild( document .querySelector( `[for=${ event .target .id }] .error` ) );
+          formulario .validaClave = true;           // Cambia el estado de validación del campo
         }
       }
 
@@ -100,10 +120,12 @@ var formulario = {
         if( !formulario .regExp .test( valor_campo_texto ) ) {
           // Hay un error en el formato de la contraseña
           document .querySelector( `[for=${ event .target .id }] .error` ) .innerHTML = '<span style="color: red;"> * Error: La cadena de texto introducida no es un correo electrónico válido</span>';
+          formulario .validaEmail = false;          // Cambia el estado de validación del campo
         }
         else {
           // Elimina elemento que contiene el mensaje de ERROR
           document .querySelector( `[for=${ event .target .id }] .error` ) .parentNode .removeChild( document .querySelector( `[for=${ event .target .id }] .error` ) );
+          formulario .validaEmail = true;           // Cambia el estado de validación del campo
         }
       }
     }
@@ -112,6 +134,36 @@ var formulario = {
       // Elimina elemento que contiene el mensaje de ERROR
       document .querySelector( `[for=${ event .target .id }] .error` ) .parentNode .removeChild( document .querySelector( `[for=${ event .target .id }] .error` ) );
     }
+  },
+  /* Función que valida el Formulario antes de ser enviado */
+  validaFormulario : function ( event ) {
+
+    formulario .validaTerminosYCondiciones = document .querySelector( '#terminos' ) .checked;
+
+    console .group( 'Valida Formulario para envio de los datos' );
+      console .log( 'usuario: ', formulario .validaUsuario );
+      console .log( 'contraseña: ', formulario .validaClave );
+      console .log( 'email: ', formulario .validaEmail );
+      console .log( 'terminos: ', formulario .validaTerminosYCondiciones );
+    console .groupEnd();
+
+    // Valida si los campos obligatorios del formulario NO han sido validados correctamente
+    if( !formulario .validaUsuario || !formulario .validaClave || !formulario .validaEmail ) {
+      event .preventDefault();                      // Evitamos que los eventos predeterminados se disparen, en nuestro caso el submit
+      console .log( 'Error Campos' );
+      document .querySelector( '#mensaje-formulario' ) .innerHTML = '<span style="color: red;">El formulario posee errores, favor revise de nuevo!</span>';
+    }
+    // Valida si NO ha checado el campo donde se ACEPTAN los "Términos y condiciones" del formulario
+    else if( !formulario .validaTerminosYCondiciones ) {
+      event .preventDefault();                      // Evitamos que los eventos predeterminados se disparen, en nuestro caso el submit
+      console .log( 'No acepto términos y condiciones' );
+      document .querySelector( '#mensaje-formulario' ) .innerHTML = '<span style="color: red;">Debe aceptar los términos y condiciones para poder enviar sus datos</span>';
+    }
+    else {
+      console .log( 'Envia los datos' );
+      document .querySelector( '#mensaje-formulario' ) .innerHTML = '<span>Los datos han sido enviados</span>';
+    }
+
   }
 }
 
