cf09111505793b7df54bd18a015987a345c5e3d1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-03-30 12:30:26 -0500

    Despliega/Oculta el menú, modifica la clase del elemento al aplicar el evento 'toogle' simulado

--- js/menu-movil.js
+++ js/menu-movil.js
@@ -8,6 +8,7 @@ var MenuMovil = {
   /* Metodo con el que se inicializan las funciones del menú */
   inicio : () => {
     this .btnMenu = document .querySelector( '#btn-movil-menu span' );          // Obtiene el elemento que representa el menú hamburguesa en el DOM
+    this .menu = document .querySelector( 'nav' );                              // Obtiene el elemento que contiene la estructura del menú principal en el DOM
 
     // Agrega evento 'click' al elemento del ícono del menú hamburguesa
     this .btnMenu .addEventListener( 'click', MenuMovil .mostrarMenu );
@@ -16,10 +17,12 @@ var MenuMovil = {
   mostrarMenu : () => {
     // Valida si el menú no esta desplegado
     if( !this .menuDesplegado ) {
-        this .menuDesplegado = true;
+        this .menuDesplegado = true;                                            // Cambia el estado
+        this .menu .className = 'col-lg-6 col-md-7 col-sm-8 col-xs-12';         // Modifica la última clase para que se despliegue el menú oculto en pantallas pequeñas
     }
     else {
-      this .menuDesplegado = false;
+      this .menuDesplegado = false;                                             // Cambia el estado
+      this .menu .className = 'col-lg-6 col-md-7 col-sm-8 col-xs-0';            // Modifica la última clase para que se oculte el menú en pantallas pequeñas
     }
     console .log( 'Menú desplegado: ', this .menuDesplegado );
   }
